a3f104b05c0461500a66cd6848355ef4985e46c3
Saksham Verma <154026243+sakshamverma2030@users.noreply.github.com>
2026-06-27 20:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.30%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int f[n+1];   // array banaya
+        f[1] = 1;
+        f[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            f[i] = f[i-1] + f[i-2];  // direct formula
+        }
+
+        return f[n];
+    }
+};

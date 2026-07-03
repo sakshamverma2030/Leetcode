ca2676cb227754afcc387e4c097ee03e2194ce28
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-03 13:54:49 +0530

    Time: 3 ms (65.44%) | Memory: 7.9 MB (37.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+        // Base cases
+        if (n == 0) return 0;   // Base case: F(0) = 0
+        if (n == 1) return 1;   // Base case: F(1) = 1
 
-        int f[n+1];   // array banaya Isliye array banaya jaata hai, taaki har index par us Fibonacci number ki value rakhi ja sake.
-        f[0] = 0;
-        f[1] = 1;
+        int prev2 = 0;  // F(0)
+        int prev1 = 1;  // F(1)
+        int cur;
 
+        // DP formula but without array
         for (int i = 2; i <= n; i++) {
-            f[i] = f[i-1] + f[i-2];  // direct formula likh diya
+            cur = prev1 + prev2;   // F(i) = F(i-1) + F(i-2)
+            prev2 = prev1;
+            prev1 = cur;
         }
-
-        return f[n];
+        return prev1;
     }
 };

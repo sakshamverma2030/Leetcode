ea09dd78eff42ba98dd96d43762430aa8131f79f
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-04 07:50:03 +0530

    Time: 0 ms (100.00%) | Memory: 74.6 MB (14.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,15 +3,15 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n);
-        
+
         dp[0] = nums[0];  // base case
-        int ans = dp[0];
-        
+        int ans = dp[0];  // initialize answer
+
         for (int i = 1; i < n; i++) {
             dp[i] = max(nums[i], dp[i-1] + nums[i]);
-            ans = max(ans, dp[i]);
+            ans = max(ans, dp[i]);   // update global max
         }
-        
-        return ans;
+
+        return ans;  // final answer
     }
 };

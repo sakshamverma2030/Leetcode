a93ae888c84cd52a07f3ca20dde1a1456353d151
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-07 13:04:57 +0530

    Time: 14 ms (23.13%) | Memory: 7.8 MB (64.67%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,15 +5,6 @@ public:
         if (n == 0) return 0;   // Base case: F(0) = 0
         if (n == 1) return 1;   // Base case: F(1) = 1
 
-        // DP array to store results
-        vector<int> dp(n+1);
-        dp[0] = 0;  // Base case
-        dp[1] = 1;  // Base case
+        return fib(n-1) + fib(n-2);
 
-        // DP formula: F(i) = F(i-1) + F(i-2)
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
     }};

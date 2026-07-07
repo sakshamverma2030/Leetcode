429b997a94895f9c4755fa5e1c2164bb848fdf6f
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-07 13:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (23.14%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -13,10 +13,12 @@ public:
         dp[1] = max(nums[0], nums[1]);  
 
         // DP formula
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) 
+        {
             dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
         }
 
         return dp[n-1];
     }
 };
+

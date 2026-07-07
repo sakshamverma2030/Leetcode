4d57d11fb68dfb336dccfba314ff15e8080c7518
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-07 10:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (55.57%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];           // primary diagonal
+            sum += mat[i][n - i - 1];   // secondary diagonal
+        }
+        
+        // subtract center element if counted twice
+        if (n % 2 == 1) {
+            sum -= mat[n/2][n/2];
+        }
+        
+        return sum;
+    }
+};

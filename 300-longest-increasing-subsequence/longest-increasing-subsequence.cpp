d9580065bf5a7e2c872934f4b6e7edda7f650c49
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-08 08:35:27 +0530

    Time: 75 ms (49.21%) | Memory: 14.3 MB (61.48%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,8 +4,15 @@ public:
         vector<int> dp(nums.size(),1);
         int ans=1;
         for(int i=0;i<nums.size();i++)
+        {
             for(int j=0;j<i;j++)
-                if(nums[j]<nums[i]) dp[i]=max(dp[i],dp[j]+1);
+            {
+                if(nums[j]<nums[i])
+                {
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
         return *max_element(dp.begin(),dp.end());
     }
 };

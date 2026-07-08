7015caf522cb4f4fd60a40cf969b9ffb28804707
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-08 08:21:02 +0530

    Time: 288 ms (22.40%) | Memory: 157.2 MB (41.02%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums) 
     {
-        int n= nums.size();
-        vector<vector<long>> dp(n+1, vector<long>(2,0)); //even= 0, odd= 1
-        for(int i = 1; i<n+1; i++)
+        int n = nums.size();
+        vector<vector<long long>> dp(n, vector<long long>(2, 0)); 
+        // dp[i][0] = max alternating sum ending at i with even length
+        // dp[i][1] = max alternating sum ending at i with odd length
+
+        dp[0][0] = 0;            // even length subsequence starting empty
+        dp[0][1] = nums[0];      // odd length subsequence starting with nums[0]
+
+        for(int i = 1; i < n; i++)
         {
-            //even, even=odd-even
-            dp[i][0]= max(dp[i-1][1]-nums[i-1],dp[i-1][0]);
-            //odd, odd= odd+even
-            dp[i][1]= max(dp[i-1][0]+nums[i-1],dp[i-1][1]);
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - nums[i]); // even
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + nums[i]); // odd
         }
-        return max(dp[n][0], dp[n][1]);
+        return max(dp[n-1][0], dp[n-1][1]);
     }
 };

bb7ab311f4ee80e3ed5bb8db110e15f440945c13
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-09 13:25:33 +0530

    Time: 34 ms (49.83%) | Memory: 27.2 MB (54.98%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -7,9 +7,12 @@ public:
         vector<int> t(n, 1);
         int maxL = 1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j][1] < nums[i][0]) {
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                if (nums[j][1] < nums[i][0])
+                {
                     t[i] = max(t[i], t[j] + 1);
                 }
             }

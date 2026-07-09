b88db5c07823d1e56563007e597f46abc32d19cc
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-09 14:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 115.6 MB (67.54%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -4,14 +4,13 @@ public:
         int first = INT_MAX;   // sabse chhota number
         int second = INT_MAX;  // second chhota number
 
-        for(int num : nums) {
-            if(num <= first) {
+        for (int num : nums) {
+            if (num <= first) {
                 first = num;   // update smallest
-            } else if(num <= second) {
+            } else if (num <= second) {
                 second = num;  // update second smallest
             } else {
-                // agar koi number second se bada mil gaya
-                return true;   // triplet exists
+                return true;   // third number mil gaya
             }
         }
         return false;

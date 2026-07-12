4186a778ed7ded89a7a5062e516eeb2660372d4e
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-12 17:32:41 +0530

    Time: 23 ms (88.52%) | Memory: 26.8 MB (43.13%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.length();  
-        int n = str2.length();   
+        int m = str1.length();   // str1 ki length nikal rahe hain
+        int n = str2.length();   // str2 ki length nikal rahe hain
 
-        vector<vector<int>> dp(m+1, vector<int>(n+1,0)); 
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0)); // DP table banaya jisme LCS store hoga
+
+        // LCS DP table fill karna
         for(int i=1; i<=m; i++) {
             for(int j=1; j<=n; j++) {
                 if(str1[i-1] == str2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1]; 
+                    dp[i][j] = 1 + dp[i-1][j-1]; // agar dono characters match karte hain toh purani kahani +1
                 } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]); 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]); // agar alag hain toh best skip choose karo
                 }
             }
         }

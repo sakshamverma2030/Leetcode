d80f30b26f6adb4dfb7f831a04d234805648beaa
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-12 10:07:13 +0530

    Time: 28 ms (43.25%) | Memory: 27.4 MB (53.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -7,11 +7,16 @@ public:
         // 2D DP table
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
 
-        for(int i=1; i<=m; i++) {
-            for(int j=1; j<=n; j++) {
-                if(s1[i-1] == s2[j-1]) {
+        for(int i=1; i<=m; i++) 
+        {
+            for(int j=1; j<=n; j++) 
+            {
+                if(s1[i-1] == s2[j-1]) 
+                {
                     dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
+                } 
+                else 
+                {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }

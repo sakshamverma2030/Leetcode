d4b5b7d74153aae1696faa9868cdc13f812f281a
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-07-13 15:29:14 +0530

    Time: 14 ms (83.04%) | Memory: 20.4 MB (67.30%) - LeetSync

---
+++ 1939-queries-on-number-of-points-inside-a-circle/queries-on-number-of-points-inside-a-circle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> ans;
+        for(auto &q : queries) {
+            int cx = q[0], cy = q[1], r = q[2];
+            int count = 0;
+            for(auto &p : points) {
+                int dx = p[0] - cx;
+                int dy = p[1] - cy;
+                if(dx*dx + dy*dy <= r*r) {
+                    count++;
+                }
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};

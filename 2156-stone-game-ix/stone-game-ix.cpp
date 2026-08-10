cb05630f9c343eddcccac4156060bfd090b62218
Saksham <154026243+sakshamverma2030@users.noreply.github.com>
2026-08-11 04:42:55 +0530

    Time: 3 ms (73.20%) | Memory: 131.4 MB (32.65%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int c0 = 0, c1 = 0, c2 = 0;
+        
+        for (int stone : stones) {
+            int rem = stone % 3;
+            if (rem == 0) c0++;
+            else if (rem == 1) c1++;
+            else c2++;
+        }
+
+        if (c0 % 2 == 0) {
+            return c1 >= 1 && c2 >= 1;
+        } else {
+            return abs(c1 - c2) >= 3;
+        }
+    }
+};
